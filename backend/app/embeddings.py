e5f9fe5e0c95550db3a80f33d1c2a6c76496c8fb
Akshay Patil <akshaypatil9714@gmail.com>
2024-08-24 13:08:43 -0400

    Remove unwanted files

--- backend/app/embeddings.py
+++
@@ -1,9 +0,0 @@
-from sentence_transformers import SentenceTransformer
-
-
-# Load the SentenceTransformer model for embeddings
-embedding_model = SentenceTransformer('all-MiniLM-L6-v2')
-
-def embed_text(text):
-    embedding = embedding_model.encode(text)
-    return embedding
