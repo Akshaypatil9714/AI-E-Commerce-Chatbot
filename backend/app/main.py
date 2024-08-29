b43b94a1345159e2454f5691ccc2cc4e42c576bb
Akshay Patil <akshaypatil9714@gmail.com>
2024-08-28 22:55:40 -0400

    Added CORS middleware

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,10 +1,23 @@
 from fastapi import FastAPI
 from app.api import router as chat_router
+import os
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
+# Add CORS middleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["https://ai-e-commerce-chatbot.vercel.app/"],  # Update this with your Vercel domain or "*" to allow all origins
+    allow_credentials=True,
+    allow_methods=["*"],  # Allow all methods (GET, POST, etc.)
+    allow_headers=["*"],  # Allow all headers
+)
+
 app.include_router(chat_router)
+
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000, debug = True)
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run(app, host="0.0.0.0", port=port, debug = True)
