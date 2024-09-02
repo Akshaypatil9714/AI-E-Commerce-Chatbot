cac480cbce01dfd4a66ec1aa80d7c7b6ad8feef1
Akshay Patil <akshaypatil9714@gmail.com>
2024-09-01 23:07:46 -0400

    The server will now act as a proxy for your FastAPI backend.

--- server/server.js
+++ server/server.js
@@ -1,13 +1,46 @@
+// const express = require("express");
+// const { createProxyMiddleware } = require("http-proxy-middleware");
+// const path = require("path");
+// require("dotenv").config();
+
+// const app = express();
+// const PORT = process.env.PORT || 8080;
+// const FASTAPI_BASE_URL = process.env.FASTAPI_BASE_URL; //|| "http://localhost:8000";
+
+// // Serve the static files from the React app
+// app.use(express.static(path.join(__dirname, "../frontend/build")));
+
+// // Proxy API requests to the FastAPI backend
+// app.use(
+//   "/api",
+//   createProxyMiddleware({
+//     target: FASTAPI_BASE_URL,
+//     changeOrigin: true,
+//     pathRewrite: {
+//       "^/api": "",
+//     },
+//   })
+// );
+
+// // Handle any other routes with the React app
+// app.get("*", (req, res) => {
+//   res.sendFile(path.join(__dirname, "../frontend/build/index.html"));
+// });
+
+// app.listen(PORT, () => {
+//   console.log(`Server is running on http://localhost:${PORT}`);
+// });
+
+
+
 const express = require("express");
 const { createProxyMiddleware } = require("http-proxy-middleware");
 const path = require("path");
+require("dotenv").config();
 
 const app = express();
-const PORT = process.env.PORT || 3001;
-const FASTAPI_BASE_URL = process.env.REACT_APP_API_BASE_URL || "http://localhost:8000";
-
-// Serve the static files from the React app
-app.use(express.static(path.join(__dirname, "../frontend/build")));
+const PORT = process.env.PORT || 8080;
+const FASTAPI_BASE_URL = process.env.FASTAPI_BASE_URL;
 
 // Proxy API requests to the FastAPI backend
 app.use(
@@ -21,9 +54,9 @@ app.use(
   })
 );
 
-// Handle any other routes with the React app
+// Handle any other routes
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../frontend/build/index.html"));
+  res.send("Server is running");
 });
 
 app.listen(PORT, () => {
